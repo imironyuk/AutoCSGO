3f642c9c67ed3b072ee729644f745cc49d622188
imiro <imironyuk1979@gmail.com>
2020-09-07 21:13:32 +0300

    //

--- Cliker/click.py
+++ Cliker/click.py
@@ -3,7 +3,8 @@ from tkinter import *
 tk = Tk()
 tk.title('Clicker')
 
-btn = Button(text='Click Me', width=15, height=7)
+btn = Button(text='Click Me', width=10, height=10)
+btn = Button(text='Click Me', width=10, height=10)
 btn.pack()
 
 n = 0 # переменная для счетчика
